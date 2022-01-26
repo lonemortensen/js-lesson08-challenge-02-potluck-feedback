b62f326741e90f1026447aeddab55a4b47c76ec3
Lone Mortensen <lmordev@gmail.com>
2022-01-25 16:10:16 -0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,88 @@
+/* POTLUCK GUEST LIST */
+
+
+// invite button
+const addGuestButton = document.querySelector(".invite");
+// label for the invite button
+const guestInputLabel = document.querySelector(".add-guest label");
+// text input box
+const guestInput = document.querySelector(".add-guest input");
+// unordered list (not yet visible)
+const guestList = document.querySelector(".guest-list");
+// span class for number of guests attending
+const guestCount = document.querySelector(".attendance");
+// alert when guest list is full (not yet visible)
+const guestFull = document.querySelector(".alert");
+
+const assignButton = document.querySelector(".assign");
+
+const assignedItems = document.querySelector(".assigned-items");
+
+//GET GUEST INPUT AND ADD LIST ELEMENT:
+addGuestButton.addEventListener("click", function () {
+  const guest = guestInput.value;
+  //console.log(guest);
+  if (guest !== "") {
+    addToList(guest);
+  }
+  clearInput();
+  updateGuestCount();
+});
+
+//CLEAR INPUT BOX:
+const clearInput = function () {
+  guestInput.value = "";
+};
+
+//REFACTOR 'ADD LIST ELEMENT':
+const addToList = function (guest) {
+  const listItem = document.createElement("li");
+  listItem.innerText = guest;
+  guestList.append(listItem);
+};
+
+//LIMIT GUEST LIST:
+const updateGuestCount = function () {
+  const guests = document.querySelectorAll(".guest-list li");
+  guestCount.innerText = guests.length;
+  if (guests.length === 8) {
+    addGuestButton.classList.add("hide");
+    guestInput.classList.add("hide");
+    guestInputLabel.classList.add("hide");
+    guestFull.classList.remove("hide");
+  }
+};
+
+//ASSIGN POTLUCK ITEMS:
+const assignItems = function () {
+  const potluckItems = [
+    "spring rolls",
+    "sandwhiches",
+    "pasta salad",
+    "green salad",
+    "salsa",
+    "sauerkraut",
+    "bread",
+    "dressing",
+    "skewers",
+    "cake",
+    "cookies",
+    "icecream"
+  ];
+  const allGuests = document.querySelectorAll(".guest-list li");
+  for (let guest of allGuests) {
+    let randomPotluckIndex = Math.floor(Math.random() * potluckItems.length);
+    let randomPotluckItem = potluckItems[randomPotluckIndex];
+
+    let listItem = document.createElement("li");
+    listItem.innerText = `${guest.innerText} is bringing ${randomPotluckItem}.`;
+    assignedItems.append(listItem);
+
+    potluckItems.splice(randomPotluckIndex, 1); // prevent assigning duplicate items.
+  }
+};
+
+assignButton.addEventListener("click", function () {
+  assignItems();
+  assignButton.disabled = true; // disable button once assignItems loop completes to avoid duplicate dish assignment. 
+});
